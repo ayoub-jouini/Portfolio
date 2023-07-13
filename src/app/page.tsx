c574fb500c0d3a78e378246c9ad42d9b38ab58d7
ayoub-jouini <ayoubjouini135@gmail.com>
2023-07-13 19:56:31 +0100

    experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,15 @@
 import CallToAction from "../components/callToAction/CallToAction";
-import DarkModeButton from "../components/buttons/DarkModeButton";
 import Header from "../components/header/Header";
-import Skills from "../components/Skills/Skills";
+import Skills from "../components/skills/Skills";
+import Experience from "../components/experience/Experience";
 
 export default function Home() {
   return (
-    <main
-      className="tracking-wider	
-"
-    >
+    <main className="tracking-wider">
       <Header />
       <CallToAction />
       <Skills />
+      <Experience />
     </main>
   );
 }
---
+++ src/components/experience/Experience.tsx
@@ -0,0 +1,81 @@
+interface Props {}
+
+const Experience: React.FC<Props> = () => {
+  return (
+    <div className="px-4 md:px-8 py-14 mx-auto lg:max-w-7xl ">
+      <h2 className="mb-16 text-center font-semibold text-4xl">Experience</h2>
+      <div className="grid grid-cols-2">
+        <div className="">
+          <div className="bg-secondary rounded-[33px] w-10/12 h-24 py-3 px-7 text-primary1 flex flex-col justify-between mb-5">
+            <h3 className="font-semibold text-xl">Project Manager</h3>
+            <div className="flex justify-between">
+              <p className="text-sm">Orenda JE</p>
+              <p className="text-sm">2022-2023</p>
+            </div>
+          </div>
+          <div className="bg-secondary rounded-[33px] w-10/12 h-24 py-3 px-7 text-primary1 flex flex-col justify-between mb-5">
+            <h3 className="font-semibold text-xl">Project Manager</h3>
+            <div className="flex justify-between">
+              <p className="text-sm">Orenda JE</p>
+              <p className="text-sm">2022-2023</p>
+            </div>
+          </div>
+          <div className="bg-secondary rounded-[33px] w-10/12 h-24 py-3 px-7 text-primary1 flex flex-col justify-between mb-5">
+            <h3 className="font-semibold text-xl">Project Manager</h3>
+            <div className="flex justify-between">
+              <p className="text-sm">Orenda JE</p>
+              <p className="text-sm">2022-2023</p>
+            </div>
+          </div>
+          <div className="bg-secondary rounded-[33px] w-10/12 h-24 py-3 px-7 text-primary1 flex flex-col justify-between mb-5">
+            <h3 className="font-semibold text-xl">Project Manager</h3>
+            <div className="flex justify-between">
+              <p className="text-sm">Orenda JE</p>
+              <p className="text-sm">2022-2023</p>
+            </div>
+          </div>
+          <div className="bg-secondary rounded-[33px] w-10/12 h-24 py-3 px-7 text-primary1 flex flex-col justify-between mb-5">
+            <h3 className="font-semibold text-xl">Project Manager</h3>
+            <div className="flex justify-between">
+              <p className="text-sm">Orenda JE</p>
+              <p className="text-sm">2022-2023</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center">
+          <div className="flex justify-between">
+            <div className="flex flex-col">
+              <h4 className="text-secondary font-bold text-2xl">
+                Project Manager
+              </h4>
+              <h5 className="text-secondary text-l">Orenda JE</h5>
+            </div>
+            <div className="border-2 border-secondary rounded-[24px] h-10 w-32 flex items-center justify-center text-secondary">
+              <p>2022-2023</p>
+            </div>
+          </div>
+          <p className="my-10">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident
+            qui optio, a laudantium quia quo corporis iste et sint repellendus
+            fugit veritatis perferendis natus inventore totam tenetur atque
+            earum. Harum!
+            <br />
+            <br />
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt
+            pariatur quos et perspiciatis dolore, illum explicabo, sit
+            temporibus doloremque ullam doloribus placeat facilis accusantium
+            unde quo nulla eum rem facere.
+            <br />
+            <br />
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt
+            pariatur quos et perspiciatis dolore, illum explicabo, sit
+            temporibus doloremque ullam doloribus placeat facilis accusantium
+            unde quo nulla eum rem facere.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
