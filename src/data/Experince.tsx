e1d10a6386b14eada4424b42edde3f38d149e4f1
ayoub-jouini <ayoubjouini135@gmail.com>
2023-07-14 03:37:25 +0100

    experience & skills

---
+++ src/data/Experince.tsx
@@ -0,0 +1,119 @@
+const experience = [
+  {
+    id: 0,
+    title: "Project Manager",
+    organisation: "Orenda JE",
+    date: "2022-2023",
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident
+    qui optio, a laudantium quia quo corporis iste et sint repellendus
+    fugit veritatis perferendis natus inventore totam tenetur atque
+    earum. Harum!
+    <br />
+    <br />
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt
+    pariatur quos et perspiciatis dolore, illum explicabo, sit
+    temporibus doloremque ullam doloribus placeat facilis accusantium
+    unde quo nulla eum rem facere.
+    <br />
+    <br />
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt
+    pariatur quos et perspiciatis dolore, illum explicabo, sit
+    temporibus doloremque ullam doloribus placeat facilis accusantium
+    unde quo nulla eum rem facere.
+`,
+  },
+  {
+    id: 1,
+    title: "Deputy Project Manager",
+    organisation: "Orenda JE",
+    date: "2021-2022",
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident
+    qui optio, a laudantium quia quo corporis iste et sint repellendus
+    fugit veritatis perferendis natus inventore totam tenetur atque
+    earum. Harum!
+    <br />
+    <br />
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt
+    pariatur quos et perspiciatis dolore, illum explicabo, sit
+    temporibus doloremque ullam doloribus placeat facilis accusantium
+    unde quo nulla eum rem facere.
+    <br />
+    <br />
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt
+    pariatur quos et perspiciatis dolore, illum explicabo, sit
+    temporibus doloremque ullam doloribus placeat facilis accusantium
+    unde quo nulla eum rem facere.
+`,
+  },
+  {
+    id: 2,
+    title: "Project Consultant",
+    organisation: "Orenda JE",
+    date: "2020-2021",
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident
+    qui optio, a laudantium quia quo corporis iste et sint repellendus
+    fugit veritatis perferendis natus inventore totam tenetur atque
+    earum. Harum!
+    <br />
+    <br />
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt
+    pariatur quos et perspiciatis dolore, illum explicabo, sit
+    temporibus doloremque ullam doloribus placeat facilis accusantium
+    unde quo nulla eum rem facere.
+    <br />
+    <br />
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt
+    pariatur quos et perspiciatis dolore, illum explicabo, sit
+    temporibus doloremque ullam doloribus placeat facilis accusantium
+    unde quo nulla eum rem facere.
+`,
+  },
+  {
+    id: 3,
+    title: "Web Developer",
+    organisation: "The DevThingz",
+    date: "Feb 2023-May 2023",
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident
+    qui optio, a laudantium quia quo corporis iste et sint repellendus
+    fugit veritatis perferendis natus inventore totam tenetur atque
+    earum. Harum!
+    <br />
+    <br />
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt
+    pariatur quos et perspiciatis dolore, illum explicabo, sit
+    temporibus doloremque ullam doloribus placeat facilis accusantium
+    unde quo nulla eum rem facere.
+    <br />
+    <br />
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt
+    pariatur quos et perspiciatis dolore, illum explicabo, sit
+    temporibus doloremque ullam doloribus placeat facilis accusantium
+    unde quo nulla eum rem facere.
+`,
+  },
+  {
+    id: 4,
+    title: "Front-end Developer",
+    organisation: "Sté Qramer",
+    date: "jul 2022-aug 2022",
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident
+    qui optio, a laudantium quia quo corporis iste et sint repellendus
+    fugit veritatis perferendis natus inventore totam tenetur atque
+    earum. Harum!
+    <br />
+    <br />
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt
+    pariatur quos et perspiciatis dolore, illum explicabo, sit
+    temporibus doloremque ullam doloribus placeat facilis accusantium
+    unde quo nulla eum rem facere.
+    <br />
+    <br />
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt
+    pariatur quos et perspiciatis dolore, illum explicabo, sit
+    temporibus doloremque ullam doloribus placeat facilis accusantium
+    unde quo nulla eum rem facere.
+`,
+  },
+];
+
+export default experience;
---
+++ src/data/Skills.tsx
@@ -0,0 +1,21 @@
+const skills = [
+  "javascript",
+  "typescript",
+  "go",
+  "java",
+  "html",
+  "nodejs",
+  "ReactJs",
+  "reactnative",
+  "nextjs",
+  "css",
+  "express",
+  "mongodb",
+  "postgress",
+  "github",
+  "docker",
+  "git",
+  "php",
+];
+
+export default skills;
