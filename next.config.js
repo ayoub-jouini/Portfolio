4805690e3282dd4630db0004a8ca5efcee664773
ayoub-jouini <ayoubjouini135@gmail.com>
2023-07-18 09:10:33 +0100

    exportPathMap

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/page": { page: "/page", fallback: false },
+      // other pages
+      // ...
+    };
+  },
+};
 
 module.exports = nextConfig;
