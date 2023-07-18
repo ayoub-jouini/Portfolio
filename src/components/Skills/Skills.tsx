eaaf339f49fc1bab4f245ed7d1bfb1dbe8416751
ayoub-jouini <ayoubjouini135@gmail.com>
2023-07-18 04:37:20 +0100

    test

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,16 +1,16 @@
-import skills from "../../data/Skills";
-import SkillIcon from "../icons/SkillIcon";
+// import skills from "../../data/Skills";
+// import SkillIcon from "../icons/SkillIcon";
 
 interface Props {}
 
 const Skills: React.FC<Props> = () => {
   return (
     <div id="skills" className="w-full bg-white2 dark:bg-primary3">
-      <div className="px-4 md:px-8 py-28 mx-auto lg:max-w-7xl grid grid-cols-2 md:grid-cols-5 justify-items-center md:gap-3 gap-16 items-center flex-wrap">
+      {/* <div className="px-4 md:px-8 py-28 mx-auto lg:max-w-7xl grid grid-cols-2 md:grid-cols-5 justify-items-center md:gap-3 gap-16 items-center flex-wrap">
         {skills.map((skill, key) => (
           <SkillIcon key={key} skill={skill} />
         ))}
-      </div>
+      </div> */}
     </div>
   );
 };
