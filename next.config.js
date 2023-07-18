6f3e5f59a6b5dc2940be304ee8062a23d7870c99
ayoub-jouini <ayoubjouini135@gmail.com>
2023-07-18 08:56:55 +0100

    config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  staticPageGenerationTimeout: 120,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/[project]": { page: "/[project]" },
+    };
+  },
 };
 
 module.exports = nextConfig;
