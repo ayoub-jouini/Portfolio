1648560c8763bf7b58e29729d6fdb0c727520706
ayoub-jouini <ayoubjouini135@gmail.com>
2023-07-19 05:14:29 +0100

    projects

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -164,7 +164,7 @@ export default function Header() {
                     isActive("projects") && "text-tertiary dark:text-secondary"
                   }`}
                 >
-                  Projets
+                  Projects
                 </a>
               </li>
               <li
